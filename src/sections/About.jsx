dc2f48a76b071ce75d5088e5b0e0f6959f9b2f92
vedant1783 <vedukasat17@gmail.com>
2025-03-31 14:10:18 +0530

    basic changes

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -25,8 +25,7 @@ const About = () => {
             <div>
               <p className="grid-headtext">Hi, I’m Vedant Kasat</p>
               <p className="grid-subtext">
-                With 12 years of experience, I have honed my skills in both frontend and backend dev, creating dynamic
-                and responsive websites.
+                "As a beginner with a growing passion for web development, I am focused on building my skills in both frontend and backend development, aiming to create dynamic and responsive websites."
               </p>
             </div>
           </div>
@@ -58,12 +57,12 @@ const About = () => {
                 showGraticules
                 globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
                 bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-                labelsData={[{ lat: 40, lng: -100, text: 'Rjieka, Croatia', color: 'white', size: 15 }]}
+                labelsData={[{ lat: 40, lng: -100, text: 'Pune,India', color: 'white', size: 15 }]}
               />
             </div>
             <div>
               <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
-              <p className="grid-subtext">I&apos;m based in Rjieka, Croatia and open to remote work worldwide.</p>
+              <p className="grid-subtext">I&apos;m based in Pune, India and open to remote work .</p>
               <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
             </div>
           </div>
